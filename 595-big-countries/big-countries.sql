320fffd5726bd1fece6b704154225ea0717669ec
theoptheod <152732897+theoptheod@users.noreply.github.com>
2025-07-19 11:40:20 +0300

    Time: 267 ms (61.31%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -3,6 +3,7 @@ SELECT
     `name`,
     `population`,
     `area`
-FROM `world`
+FROM `World`
 WHERE (`area` >= 3000000)
-OR (`population` >= 25000000);
+OR (`population` >= 25000000)
+ORDER BY `name` ASC;
